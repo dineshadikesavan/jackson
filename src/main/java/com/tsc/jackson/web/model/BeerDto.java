843aa6bf2ff223c1d079ff78d933a44358c5b952
Dinesh A <dinesh.adikesavan@gmail.com>
2024-12-08 08:57:56 -0600

    initial commit

---
+++ src/main/java/com/tsc/jackson/web/model/BeerDto.java
@@ -0,0 +1,40 @@
+package com.tsc.jackson.web.model;
+
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Null;
+import jakarta.validation.constraints.Positive;
+import jakarta.validation.constraints.Size;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.math.BigDecimal;
+import java.time.OffsetDateTime;
+import java.util.UUID;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class BeerDto {
+
+    @Null
+    private UUID id;
+    private Integer version;
+    private OffsetDateTime createdDate;
+
+    private OffsetDateTime lastModifiedDate;
+
+    @NotBlank
+    @Size(min = 3, max = 50)
+    private String beerName;
+
+    @NotBlank
+    private String beerStyle;
+
+    @Positive
+    private Long upc;
+    private BigDecimal price;
+    private Integer quantityOnHand;
+}
